a04e82ba527d94853da570119307e910a8e4d5f8
shashankgaur1308@gmail.com <shashankgaur1308@gmail.com>
2025-02-23 12:36:50 +0530

    Second commit

--- index.js
+++ index.js
@@ -17,6 +17,22 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use(cors());
+
+const allowedOrigins = ["https://recipesnest.netlify.app"];
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, origin);
+        } else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+}));
+app.options('*', cors());
+
 app.use(cors({
     origin: '*',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
